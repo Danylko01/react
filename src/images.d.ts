8fd8d4cffcea154acef849bb20f287408fbf3c0e
Danylko <guanghua_dan@163.com>
2024-08-26 20:08:43 +0800

    react项目初始化

---
+++ src/images.d.ts
@@ -0,0 +1,15 @@
+/*
+ * @Description: 
+ * @Author: Danylko
+ * @Date: 2024-08-26 19:01:50
+ * @LastEditTime: 2024-08-26 19:02:11
+ */
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
+declare module '*.less'
+declare module '*.css'
